a6cb2aef7cab2ee6ef4db6c30e8386a1995fb6ff
kat-rng <92893261+kat-rng@users.noreply.github.com>
2022-03-19 19:24:55 -0400

    Find Angle and Length, .asv to gitignore
    Added functions to find angle and length and variables to store the initial and final length and angle values. Added all *.asv files to gitignore

--- connectorLine.m
+++ connectorLine.m
@@ -3,7 +3,10 @@ classdef connectorLine
     properties
         point1;
         point2;
-        length;
+        lengthInit;
+        lengthNow;
+        angleInit;
+        angleNow;
         material;
         materialStrength;
     end
@@ -13,8 +16,13 @@ classdef connectorLine
         function obj = connectorLine(point1, point2, mat, materialStr)
             obj.point1 = point1;
             obj.point2 = point2;
-
-            obj.length = sqrt((point1.x - point2.x)^2 + (point1.y - point2.y)^2);
+            
+            % Setting up initial values
+            obj.lengthInit = findLength(point1, point2);
+            obj.lengthNow = obj.lengthInit;
+            obj.angleInit = findAngle(point1, point2);
+            obj.angleNow = obj.angleInit;
+            
             obj.material = mat;
             obj.materialStrength = materialStr;
         end
@@ -29,6 +37,15 @@ classdef connectorLine
             catch
                 delLine = 1;
             end
+        end
+        
+        function dist = findLength(point1, point2)
+            % Literally the pythagorean theorem
+            dist = sqrt((point1.x - point2.x)^2 + (point1.y - point2.y)^2);
+        end
+        function angle = findAngle(point1, point2)
+            % Opposite over adjacent, finds angle above x axis
+            angle = atan((point1.y - point2.y)/(point1.x - point2.x));
         end
     end
 end
