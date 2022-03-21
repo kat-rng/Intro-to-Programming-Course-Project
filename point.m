ee4f745ccd140d894f5e94e3de850c04fb83f437
kat-rng <92893261+kat-rng@users.noreply.github.com>
2022-03-21 15:54:39 -0400

    Added tons of comments and movement functions

--- point.m
+++ point.m
@@ -3,24 +3,55 @@ classdef point
         %position
         x = 0;
         y = 0;
-        fxd = false;
+        fxdA = false;
+        fxdPt = false;
         ptID;
+        vX = 0;
+        vY = 0;
+        mass = 0;
 
         %connected 
         cLines = []; 
+        
+        %should be 1 for first point on the line, 2 for second point on the
+        %line
+        cLinesNo = [];
     end
     
     % Add Methods
     methods
         % Constructors
-        function obj = point(x, y, fxd, id)
+        function obj = point(x, y, fxdPt, fxdA, id)
             %The x and y coordinates of the point
             obj.x = x;
             obj.y = y;
             obj.ptID = id;
             
             %whether the object is fixed
-            obj.fxd = fxd;
+            obj.fxdA = fxdA;
+            obj.fxdPt = fxdPt;
+            
+            obj.mass = 
+        end
+        
+        function velocity = move()
+            % 1 is x, 2 is y
+            velocity(1) = 
+        end
+        
+        function xy = sumF(self)
+            % standard xy format
+            xy = zeros(2,1);
+            
+            % iterate through cLines and add the forceToXY from each
+            for i = 1:length(self.cLines)
+                xy = xy + self.cLines(i).totalForce(self.cLines(i), self.cLinesNo(i));
+            end
+        end
+        
+        function dv = changeVelocity()
+            dv(1) = 
+            dv(2) = 
         end
     end
 
@@ -35,15 +66,22 @@ classdef point
             end
         end
         
-        function xy = forceToXY(forces, angles, xy)
+        function ptMass = sumMass(lines)
+            ptMass = 0;
+            for i = 1:length(lines)
+                ptMass + lines(i).mass;
+            end
+        end
+        
+        function xy = forceToXY(forces, angles)
             % obtains the components of the forces
             for i = 1:length(forces)
-                xy(1:2) = 0;
+                xy = zeros(2,1);
                 
                 %using trigonometry to find the sum of the two components 
                 %of the forces
                 xy(1) = xy(1) + forces(i)*cos(angles(i));
-                xy(2) = xy(2) + forces(i)*sin(angles(i));                
+                xy(2) = xy(2) + forces(i)*sin(angles(i));
             end
         end
     end
