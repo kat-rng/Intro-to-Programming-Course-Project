24785624c6a307fb20c8d5655d97992379451443
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2022-03-21 17:16:35 -0400

    Added bridgeMaterial class. Updated addLine and main functions. Updated connectorLine class.

---
+++ bridgeMaterial.m
@@ -0,0 +1,25 @@
+classdef bridgeMaterial
+    properties
+        name;
+        matID;
+        density;
+        maxShear;
+        maxAxial;
+        youngsModulus;
+        shearModulus;
+    end
+
+    methods
+        % Constructor
+        function obj = bridgeMaterial(name, density, maxShear, maxAxial, youngsMod, shearMod, id)
+            obj.name = name;
+            obj.density = density;
+            obj.maxShear = maxShear;
+            obj.maxAxial = maxAxial;
+            obj.youngsModulus = youngsMod;
+            obj.shearModulus = shearMod;
+            obj.matID = id;
+        end
+
+    end
+end
