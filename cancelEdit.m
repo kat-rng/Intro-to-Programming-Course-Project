a3cfc28a7e03f5b33d10ee4f6d61a0e95afb2535
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2022-03-27 16:10:30 -0400

    Removed cancelEdit function - replaced by easier method.

--- cancelEdit.m
+++
@@ -1,9 +0,0 @@
-function cancelEdit(figure)
-    % Closes the editting point/line window, not applying any changes to
-    % existing points and lines.
-    close(figure);
-
-
-    % TO BE DELETED
-    
-end
