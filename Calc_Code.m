733d1a14c9bf63587a65fb3b8591e56d88ef1972
kat-rng <92893261+kat-rng@users.noreply.github.com>
2022-04-01 15:00:05 -0400

    Added movePoints and accelPoints to Calc_Code
    Added the basic functions to shift around the points and alter their velocities

--- Calc_Code.m
+++ Calc_Code.m
@@ -9,6 +9,17 @@ lines = zeros(1,8);
 
 forces = zeros(1,0);
 
-function points = movePoints(points)
+function points = movePoints(points, timeStep)
+    for i = 1:length(points(:,1))
+        points(i,4) = points(i,4) + points(i,6)*timeStep;
+        points(i,5) = points(i,5) + points(i,7)*timeStep;
+    end
+end
+function points = accelPoints(points, timeStep)
+    for i = 1:length(points(:,1))
+        points(i,6) = points(i,6) + points(i,8)*timeStep/points(i,10);
+        points(i,7) = points(i,7) + points(i,9)*timeStep/points(i,10);
+    end
+end
     
 end
