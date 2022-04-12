3338ff54557998577f4e4a4f93c20d82bbd9463e
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2022-04-11 22:06:39 -0400

    Cleared out points and lines on stopSimulation
    Ensured values were cleared when the simulation ends.

--- stopSimulation.m
+++ stopSimulation.m
@@ -2,6 +2,10 @@ function stopSimulation(buttonSrc, mainAxes, resultAxes, simulationDisplays)
     % Runs when the simulation is stopped.
 
     global isStarting;
+    global points;
+    points = zeros(1,1);
+    global lines;
+    lines = zeros(1,1);
 
     % Reset elements to be hidden and switches from result graph to input.
     isStarting = 0;
