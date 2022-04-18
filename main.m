13278c2b3bace889b26b95e27bde281bb17128ad
kat-rng <92893261+kat-rng@users.noreply.github.com>
2022-04-18 16:50:31 -0400

    Added range for TimeStep slider to main
    Added a 2x1 matrix which can be modified to change the timeStepRange

--- main.m
+++ main.m
@@ -33,6 +33,7 @@ allPlottedLines = [];
 ptIDCount = 0;
 lnIDCount = 0;
 editPresent = 0;
+timeStepRange = [0.0001, 0.05];
 
 %% Generate Materials
 %name, density, maxShear, maxAxial, youngs, shearMod, id
@@ -116,7 +117,7 @@ helpButton = uicontrol(mainWindow, "Style", "pushbutton", "String", "Help", "Pos
 closeButton = uicontrol(mainWindow, "Style", "pushbutton", "String", "Close", "Position", [0.825 * appWidth, 0.08 * appHeight, 0.15 * appWidth, 0.05 * appHeight], "FontSize", buttonFontSize, "BackgroundColor", "#8BA7A9");
 
 % Time Step Slider
-timeSlider = uicontrol("Parent", mainWindow, "Style", "slider", "Position", [0.15 * appWidth, 0.03 * appHeight, 0.5 * appWidth, 0.025 * appHeight], "Value", 0.001, "Max", 0.05, "Min", 0.001, "BackgroundColor", "#8BA7A9");
+timeSlider = uicontrol("Parent", mainWindow, "Style", "slider", "Position", [0.15 * appWidth, 0.03 * appHeight, 0.5 * appWidth, 0.025 * appHeight], "Value", timeStepRange(1), "Max", timeStepRange(2), "Min", timeStepRange(1), "BackgroundColor", "#8BA7A9");
 timeDisp = uicontrol("Parent", mainWindow, "Style", "text", "Position", [0.32 * appWidth, 0.055 * appHeight, 0.16 * appWidth, 0.025 * appHeight], "String", "Timestep: " + num2str(timeSlider.Value));
 timeStep = timeSlider.Value;
 addlistener(timeSlider, "Value", "PostSet", @(~, ~) getTimeStep(timeSlider, timeDisp));
